4d412a4747d0e0f8c369015559e6969c83114b9f
Sayam2801 <sayamk565@gmail.com>
2021-03-24 01:03:48 +0530

    first-commit

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue); //Saving the value of text input...
+  }
+
+  function handleClick() {
+    setItems((prevItems) => [...prevItems, inputText]);
+    setInputText(""); //Restoring text input
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={inputText} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((toDoItem) => (
+            <li>{toDoItem}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
